93e7c71fb6d368eb7ac151ff03127ec0235dd48d
Nils Wirsing <nils.wirsing@outlook.de>
2023-08-12 22:11:23 +0200

    Added menu functionality

---
+++ script.ps1
@@ -0,0 +1,83 @@
+# Menus
+
+function Show-CustomMenu
+{
+    param (
+        [string]$menuname = 'Select an option'
+    )
+
+    Clear-Host
+    
+    Write-Host "1: Download Stuff"
+    Write-Host "2: Optimization Stuff"
+    Write-Host "3: Submenu 2"
+    Write-Host "4: "
+    Write-Host "Q: Exit"
+    Write-Host ""
+    Write-host ""
+}
+
+function Show-DownloadMenu
+{
+    Clear-Host
+
+    Write-Host "1: Drivers"
+    Write-Host "2: Software"
+    # Write-Host "3: idk yet"
+    # Write-Host "4: idk yet"
+    Write-Host "B: Back to Main Menu"
+
+}
+
+function Show-OptimizationMenu
+{
+
+    Clear-Host
+
+    Write-Host "1: Registry Stuff"
+    Write-Host "2: Clean Temp Files"
+    Write-Host "B: Back to Main Menu"
+}
+
+# Show main menu
+Show-CustomMenu
+
+do {
+    $select = Read-Host "Select an option: "
+
+    switch ($select){
+        '1' {
+            Show-DownloadMenu
+            do {
+                $downloadOption = Read-Host "Select a download option: "
+                switch ($downloadOption) {
+                    '1' { 'Downloading something (download 1)' }
+                    '2' { 'Downloading something (download 2)' }
+                    'q' {exit}
+                }
+            } while ($downloadOption -ne 'B')
+            Show-CustomMenu
+        }
+        '2' {
+            Show-OptimizationMenu
+            do {
+                $optimizationOption = Read-Host "Select an option: "
+                switch ($optimizationOption) {
+                    '1' { 'Registry Tweaks Menu coming soon.' }
+                    '2' { 'Clean Temp Files option coming soon.' }
+                    'q' {exit}
+                }
+            } while ($optimizationOption -ne 'B')
+            Show-CustomMenu
+        }
+        '3' {
+            # Submenu 2 logic
+        }
+        '4' {
+            # Submenu 3 logic
+        }
+        'q' {
+            exit
+        }
+    }
+} while ($true)
